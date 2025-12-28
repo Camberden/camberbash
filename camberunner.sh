b97e50b547487962119ac57e6e29b8ee314a803c
Camberden <42500247+Camberden@users.noreply.github.com>
2025-12-28 07:16:50 -0500

    Added effects; corrected concerns; steamlined and condensed content

--- camberunner.sh
+++ camberunner.sh
@@ -1,182 +1,5 @@
 #! /bin/bash
 timerunner () {
-	rt=$(tput sgr0); r=$(tput setaf 1); g=$(tput setaf 2); y=$(tput setaf 3); c=$(tput setaf 6); b=$(tput bold); start="$(date +%s)" ;
-	echo "${b}${r}[${rt}${b}${y}$(date +"%m-%d-%Y %H:%M:%S")${b}${r}]${rt}" ;
-	echo "${g}Working from${rt} ${b}${c}${PWD}${rt}" ; 
-	echo "${c}as${rt} ${b}${g}${USER} with Bash Version ${BASH_VERSION}${rt}" "${b}${y}...${rt}" ;
-	sleep 1 ;  
-	echo "$start" ;
-}
-
-cyan-echo () {
-	tput setaf 6 ; echo "$1" ; tput sgr0 ;
-}
-yellow-echo () {
-	tput setaf 3 ; echo "$1" ; tput sgr0 ;
-}
-green-echo () {
-	tput setaf 2 ; echo "$1" ; tput sgr0 ;
-}
-pink-echo () {
-	tput setaf 5 ; echo "$1" ; tput sgr0 
-}
-
-closeout () {
-	rt=$(tput sgr0) ; 
-	r=$(tput setaf 1); 
-	g=$(tput setaf 2); 
-	y=$(tput setaf 3); 
-	c=$(tput setaf 6); 
-	b=$(tput bold) ;
-	echo "${b}${r}[${rt}${b}${y}Closing out${b}${r}]${rt}${b}${c}=====> ${rt}" ; 
-	echo "${b}${r}[====> Ended =====> ${rt} ${b}${y}...${rt}" ;
-	unset IFS ;
-	if $1 ; then
-	exit 0;
-fi
-
-}
-
-IFS=,;
-local_user=$USER \
-yq -i '.configuration.meta.local-user = env(local_user)' runner.yaml ; 
-local_dir=$(pwd)/ \
-yq -i '.configuration.meta.local-dir = env(local_dir)' runner.yaml ;
-
-local_user="$( yq '.configuration.meta.local-user' runner.yaml)" ;
-local_dir="$( yq '.configuration.meta.local-dir' runner.yaml)" ;
-remote_user="$( yq '.configuration.meta.remote-user' runner.yaml)" ;
-remote_host="$( yq '.configuration.meta.remote-host' runner.yaml)"
-remote_dir="$( yq '.configuration.meta.remote-dir' runner.yaml)"
-dry_run="$( yq '.configuration.meta.dry-run' runner.yaml)" ;
-remote_path="$remote_user$remote_host:$remote_dir" ;
-
-declare -a meta_values ;
-yaml_args="$( yq '.configuration.meta.[]' runner.yaml | tr '\n' ', ')" ;
-meta_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra meta_values <<< "$meta_args" ; unset yaml_args ;
-declare -a git_values ;
-yaml_args="$( yq '.configuration.git-args[]' runner.yaml | tr '\n' ', ')" ;
-git_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra git_values <<< "$git_args" ; unset yaml_args ;
-declare -a rsync_values ;
-yaml_args="$( yq '.configuration.rsync-args[]' runner.yaml | tr '\n' ', ')" ;
-rsync_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra rsync_values <<< "$rsync_args" ; unset yaml_args ;
-declare -a exclusion_values ;
-yaml_args="$( yq '.configuration.rsync-args.rsync-exclusions.[]' runner.yaml | tr '\n' ', ' )" ;
-exclusion_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra exclusion_values <<< "$exclusion_args" ; unset yaml_args ;
-
-# cyan-echo "=====> TESTING YAML VALUES IFS =====>" ;
-# (IFS=,; echo "${meta_values[*]}"; unset $IFS) ;
-# (IFS=,; echo "${git_values[*]}"; unset $IFS) ;
-# (IFS=,; echo "${rsync_values[*]}"; unset $IFS) ;
-# (IFS=,; echo "${exclusion_values[*]}"; unset $IFS) ;
-
-git_add () {
-	if $1 ; then
-		cyan-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====>" ;
-		git status ;
-		git add . --dry-run ;
-	else
-		yellow-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====>" ;
-		git status ;
-		git add . ;
-	fi
-}
-
-git_commit () {
-	if $1 ; then
-		git commit -m "$2" --dry-run ;
-	else
-		git commit -m "$2" ;
-	fi
-}
-
-rsync_push () {
-
-	if $1 ; then
-		
-		cyan-echo "=====> DRY GIT PUSHED THE FOLLOWING: =====>" ;
-		git push --dry-run ;
-		cyan-echo "=====> DRY RSYNCED TO SERVER: =====>" #-havzun
-		rsync -havzune 'ssh -p 6543' --exclude={'*.yaml','*.git','*.sh','*.gitignore','.DS_Store'} ./* "$remote_path" ;
-		# rsync -chavznP -e "ssh -p 6543" camberde@130.51.180.241:ncco.us/ /Users/chrispy/Documents/ncco/ncco.us/
-		cyan-echo "=====> DRY COMMIT, PUSH, & RSYNC COMPLETE =====>/" ;
-		closeout ;
-	else
-		cyan-echo "=====> GIT PUSHED THE FOLLOWING: =====> "
-		git push ;
-		cyan-echo "=====> DRY RSYNCED TO SERVER: =====>" #-havzun
-		rsync -havzun 'ssh -p 6543' --exclude={'*.yaml','*.git','*.sh','*.gitignore','.DS_Store'} ./* "$remote_path" ;
-		yellow-echo "=====> COMMIT, PUSH, & RSYNC COMPLETE =====>" ;
-		closeout ;
-	fi
-}
-
-yellow-echo "SOURCE DIRECTORY =====> $local_dir" ;
-yellow-echo "TARGET DIRECTORY =====> $remote_path" ;
-for X in bash git yq
-do
-	echo -n "$X =====> "; which $X ;
-done
-
-git_add "$dry_run" ;
-cyan-echo "=====> Enter your commit message, " "$local_user." ;
-cyan-echo "[Cancels under 3 characters.] =====>" ;
-
-if [ "$dry_run" ]; then
-pink-echo "[Press w to enable wet run (overwrite mode)] =====>" ;
-else 
-pink-echo "[Press d to enable dry run (safe mode)] =====>" ;
-fi
-printf '%s' "..."; read -r message ;
-
-message="$(printf '%s' "$message")" ;
-
-if [ "$dry_run" ] && [ "$message" == "w" ] ; then
-	yellow-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====>" ;
-	git reset ;
-	pink-echo "=====> Rerun enabled utility. Exiting..." ;
-	yq -i .configuration.meta.dry-run=false runner.yaml ;
-	closeout true ;
-fi
-
-if [ "$dry_run" == false ] && [ "$message" == "d" ] ; then
-	green-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====>";
-	git reset ;
-	pink-echo "=====> Rerun utility for safe testing. Exiting..." ;
-	yq -i .configuration.meta.dry-run=true runner.yaml ;
-	closeout true ;
-fi 
-
-if [ ${#message} -gt 2 ] ; then
-	green-echo "[SUCCESS] =====>"
-	commit=$message \
-	yq -i '.configuration.meta.latest-commit = env(commit)' runner.yaml ;
-
-git_commit "$dry_run" "$message" ;
-
-else
-	git reset ;
-	echo "=====> Too few characters (# < 3) Exiting..."
-	echo "=====> Commit cancelled..." ; closeout true;
-fi
-
-cyan-echo "=====> Push data?"
-cyan-echo "[Press p to confirm] =====>"
-read -r pushed
-
-if [ "$pushed" == "p" ] ; then
-echo "Data pushing: " ; pink-echo "$message" ;
-rsync_push "$dry_run" ;
-else
-git reset ;
-cyan-echo "=====> Push cancelled..." ; closeout ;
-fi
-#! /bin/bash
-timerunner () {
 	rt=$(tput sgr0); r=$(tput setaf 1); g=$(tput setaf 2); y=$(tput setaf 3); c=$(tput setaf 6); b=$(tput bold);
 	start="$(date +%s)"
 	echo "${b}${r}[${rt}${b}${y}$(date +"%m-%d-%Y %H:%M:%S")${b}${r}]${rt}" ;
@@ -186,6 +9,11 @@ timerunner () {
 	echo "$start" ;
 }
 timerunner
+lang-paths () {
+	for X in bash git yq ; do			
+	echo -n "$X =====> "; which $X ;
+	done
+}
 cyan-echo () {
 	tput setaf 6 ; echo "$1" ; tput sgr0 ;
 }
@@ -199,20 +27,89 @@ pink-echo () {
 	tput setaf 5 ; echo "$1" ; tput sgr0 
 }
 closeout () {
+	#@ $1 Closeout? Rerun? true | false
 	rt=$(tput sgr0) ; 
 	r=$(tput setaf 1); 
 	g=$(tput setaf 2); 
 	y=$(tput setaf 3); 
 	c=$(tput setaf 6); 
 	b=$(tput bold) ;
-	echo "${b}${r}[${rt}${b}${y}Closing out${b}${r}]${rt}${b}${c}=====> ${rt}" ; 
-	echo "${b}${r}[====> Ended =====> ${rt} ${b}${y}...${rt}" ;
 	unset IFS ;
 
 	if $1 ; then
-	exit 0;
+		echo "${b}${r}[${rt}${b}${y}Closing out${b}${r}]${rt}${b}${c}=====> ${rt}" ; 
+		build-echo "[====> Ended =====> " 1;
+		# echo "${b}${r}[====> Ended =====> ${rt} ${b}${y}...${rt}" ;
+		exit 0;
 	fi
+
+	exec bash "$0" "$@" ;
 }
+build-echo() {
+	#@ $1 $string()
+	#@ $2 $color
+	rt=$(tput sgr0) ; 
+	buildstr=$1
+	for (( i=0; i<${#buildstr}; i++ )); do
+		# j=$(( i % 4 )) ;
+		c=$(tput setaf "${2}") ;
+		u=$(tput smul) ;
+		printf '%s' "${u}${c}${buildstr:$i:1}${rt}" ;
+		sleep 0.01 ;
+	done
+	sleep 0.5;
+}
+build-slowly() {
+	#@ $1 $string()
+	rt=$(tput sgr0) ; 
+	buildstr=$1
+	for (( i=0; i<${#buildstr}; i++ )); do
+		j=$(( i / 7 )) ;
+		c=$(tput setaf "${j}") ;
+		u=$(tput bold) ;
+		printf '%s' "${u}${c}${buildstr:$i:1}${rt}" ;
+		sleep 0.2 ;
+	done
+	sleep 0.5;
+	echo ;
+}
+notes-do () {
+	
+	echo "
+	# 0	Black
+	# 1	Red
+	# 2	Green
+	# 3	Yellow
+	# 4	Blue
+	# 5	Magenta
+	# 6	Cyan
+	# 7	White
+	# 8	Not used
+	# 9	Reset to default color
+
+	# sc	Save the cursor position
+	# rc	Restore the cursor position
+	# home	Move the cursor to upper left corner (0,0)
+	# cup <row> <col>	Move the cursor to position row, col
+	# cud1	Move the cursor down 1 line
+	# cuu1	Move the cursor up 1 line
+	# civis	Set to cursor to be invisible
+	# cnorm	Set the cursor to its normal state
+
+	# bold	Start bold text
+	# smul	Start underlined text
+	# rmul	End underlined text
+	# rev	Start reverse video
+	# blink	Start blinking text
+	# invis	Start invisible text
+	# smso	Start “standout” mode
+	# rmso	End “standout” mode
+	# sgr0	Turn off all attributes
+	# setaf <value>	Set foreground color
+	# setab <value>	Set background color
+	"
+}
+
 
 IFS=,;
 local_user=$USER \
@@ -220,50 +117,43 @@ yq -i '.configuration.meta.local-user = env(local_user)' runner.yaml ;
 local_dir=$(pwd)/ \
 yq -i '.configuration.meta.local-dir = env(local_dir)' runner.yaml ;
 
+#@ yaml_value="$( yq '.configuration._____' runner.yaml)" ;
 local_user="$( yq '.configuration.meta.local-user' runner.yaml)" ;
 local_dir="$( yq '.configuration.meta.local-dir' runner.yaml)" ;
 remote_user="$( yq '.configuration.meta.remote-user' runner.yaml)" ;
 remote_host="$( yq '.configuration.meta.remote-host' runner.yaml)"
 remote_dir="$( yq '.configuration.meta.remote-dir' runner.yaml)"
 dry_run="$( yq '.configuration.meta.dry-run' runner.yaml)" ;
+rsync_enabled="$( yq '.configuration.rsync-args.rsync-enabled' runner.yaml)" ;
 remote_path="$remote_user$remote_host:$remote_dir" ;
 
-declare -a meta_values ;
-yaml_args="$( yq '.configuration.meta.[]' runner.yaml | tr '\n' ', ')" ;
-meta_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra meta_values <<< "$meta_args" ; unset yaml_args ;
-declare -a git_values ;
-yaml_args="$( yq '.configuration.git-args[]' runner.yaml | tr '\n' ', ')" ;
-git_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra git_values <<< "$git_args" ; unset yaml_args ;
-declare -a rsync_values ;
-yaml_args="$( yq '.configuration.rsync-args[]' runner.yaml | tr '\n' ', ')" ;
-rsync_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra rsync_values <<< "$rsync_args" ; unset yaml_args ;
-declare -a exclusion_values ;
-yaml_args="$( yq '.configuration.rsync-args.rsync-exclusions.[]' runner.yaml | tr '\n' ', ' )" ;
-exclusion_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
-IFS=,; read -ra exclusion_values <<< "$exclusion_args" ; unset yaml_args ;
-
-cyan-echo "=====> TESTING YAML VALUES IFS =====>" ;
-(IFS=,; echo "${meta_values[*]}"; unset $IFS) ;
-(IFS=,; echo "${git_values[*]}"; unset $IFS) ;
-(IFS=,; echo "${rsync_values[*]}"; unset $IFS) ;
-(IFS=,; echo "${exclusion_values[*]}"; unset $IFS) ;
-
 git_add () {
+	#@ $1 Dry Run? true | false
+	#@ $2 Rsync Enabled? true | false
 	if $1 ; then
-		cyan-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====>" ;
+		build-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====> " 2;
+		echo ;
 		git status ;
 		git add . --dry-run ;
 	else
-		yellow-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====>" ;
+		build-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====> " 3;
+		echo ;
 		git status ;
 		git add . ;
+	fi
+
+	if $2 ; then
+		build-echo "=====> [RSYNC ENABLED] =====> " 3;
+		echo ;
+	else
+		build-echo "=====> [RSYNC DISABLED] =====> " 2;
+		echo ;
 	fi
 }
 
 git_commit () {
+	#@ $1 Dry Run? true | false
+	#@ $2 Message? string() from $message
 	if $1 ; then
 		git commit -m "$2" --dry-run ;
 	else
@@ -272,65 +162,77 @@ git_commit () {
 }
 
 rsync_push () {
-
-	if $1 ; then
-		
+	#@ $1 Dry Run? true | false
+	#@ $2 Rsync? true | false
+	if $1 ; then 
 		cyan-echo "=====> DRY GIT PUSHED THE FOLLOWING: =====>" ;
 		git push --dry-run ;
-		cyan-echo "=====> DRY RSYNCED TO SERVER: =====>" #-havzun
+		yellow-echo "=====> DRY COMMIT & PUSH COMPLETE =====>" ;
+		if $2 ; then
+		cyan-echo "=====> DRY RSYNCED TO SERVER: =====> " ; #-havzune
 		rsync -havzune 'ssh -p 6543' --exclude={'*.yaml','*.git','*.sh','*.gitignore','.DS_Store'} ./* "$remote_path" ;
-		# rsync -chavznP -e "ssh -p 6543" camberde@130.51.180.241:ncco.us/ /Users/chrispy/Documents/ncco/ncco.us/
 		cyan-echo "=====> DRY COMMIT, PUSH, & RSYNC COMPLETE =====>/" ;
+		fi
 		closeout ;
 	else
 		cyan-echo "=====> GIT PUSHED THE FOLLOWING: =====> "
 		git push ;
-		cyan-echo "=====> DRY RSYNCED TO SERVER: =====>" #-havzun
-		rsync -havzun 'ssh -p 6543' --exclude={'*.yaml','*.git','*.sh','*.gitignore','.DS_Store'} ./* "$remote_path" ;
-		yellow-echo "=====> COMMIT, PUSH, & RSYNC COMPLETE =====>" ;
+		yellow-echo "=====> GIT COMMIT & PUSH COMPLETE =====>" ;
+		if $2 ; then
+		cyan-echo "=====> DRY RSYNCED TO SERVER: =====> " ; #-havzue
+		rsync -havzue 'ssh -p 6543' --exclude={'*.yaml','*.git','*.sh','*.gitignore','.DS_Store'} ./* "$remote_path" 
+		yellow-echo "=====> RSYNC COMPLETE =====>" ;
+		fi
 		closeout ;
 	fi
 }
 
+rsync_pull () {
+	#@ $1 Dry Run? true | false
+	#@ $2 Rsync Pull? true | false
+	if $1 && $2 ; then
+		cyan-echo "=====> DRY RSYNCED PULLED FROM SERVER: =====> " ; #-havzune
+			rsync -chavznP -e "ssh -p 6543" "$remote_path" ./* 
+	fi
+	
+
+}
+
 yellow-echo "SOURCE DIRECTORY =====> $local_dir" ;
 yellow-echo "TARGET DIRECTORY =====> $remote_path" ;
-for X in bash git yq
-do
-	echo -n "$X =====> "; which $X ;
-done
 
-git_add "$dry_run" ;
-cyan-echo "=====> Enter your commit message, " "$local_user." ;
-cyan-echo "[Cancels under 3 characters.] =====>" ;
+git_add "$dry_run" "$rsync_enabled" ;
+build-echo "=====> Enter your commit message, " 6 ; build-slowly "$local_user". ;
+cyan-echo "[Cancels under 3 characters.] =====> " ;
 
 if "$dry_run" ; then
-pink-echo "[Press w to enable wet run (overwrite mode)] =====>" ;
+build-echo "[Press w to enable wet run (overwrite mode)] =====> " 5;
 else 
-pink-echo "[Press d to enable dry run (safe mode)] =====>" ;
+build-echo "[Press d to enable dry run (safe mode)] =====> " 5;
 fi
-printf '%s' "..."; read -r message ;
 
-message="$(printf '%s' "$message")" ;
+read -r message ;
+message="$(printf '%s' "$message" )" ;
 
 if [ "$dry_run" ] && [ "$message" == "w" ] ; then
-	yellow-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====>";
+	build-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====> " 3;
 	git reset ;
 	pink-echo "=====> Rerun enabled utility. Exiting..." ;
 	yq -i .configuration.meta.dry-run=false runner.yaml
-	closeout true ;
+	closeout false ;
 fi
 
 if [ "$dry_run" == false ] && [ "$message" == "d" ] ; then
-	green-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====>";
+	build-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====> " 2;
 	git reset ;
 	pink-echo "=====> Rerun utility for safe testing. Exiting..." ;
 	yq -i .configuration.meta.dry-run=true runner.yaml
-	closeout true ;
+	closeout false ;
 fi 
 
 if [ ${#message} -gt 2 ] ; then
-	echo "[SUCCESS] =====>"
-	commit=$message \
+	echo "[SUCCESS] =====> "
+	commit=$message\
 	yq -i '.configuration.meta.latest-commit = env(commit)' runner.yaml ;
 
 git_commit "$dry_run" "$message" ;
@@ -341,13 +243,13 @@ else
 	echo "=====> Commit cancelled..." ; closeout true;
 fi
 
-cyan-echo "=====> Push data?"
-cyan-echo "[Press p to confirm] =====>"
+cyan-echo "=====> Push data? "
+build-echo "[Press p to confirm] =====> " 6;
 read -r pushed
 
 if [ "$pushed" == "p" ] ; then
 echo "Data pushing: " ; pink-echo "$message" ;
-rsync_push "$dry_run" ;
+rsync_push "$dry_run" "$rsync_enabled";
 else
 git reset ;
 cyan-echo "=====> Push cancelled..." ; closeout ;
--- excess.sh
+++ excess.sh
@@ -1,5 +1,47 @@
 #! /bin/bash
 
+flourish () {
+	DATA[0]="     _/  _/    _/                            _/    "
+	DATA[1]="  _/_/_/_/_/  _/_/_/      _/_/_/    _/_/_/  _/_/_/ "
+	DATA[2]="   _/  _/    _/    _/  _/    _/  _/_/      _/    _/"
+	DATA[3]="_/_/_/_/_/  _/    _/  _/    _/      _/_/  _/    _/ "
+	DATA[4]=" _/  _/    _/_/_/      _/_/_/  _/_/_/    _/    _/  "
+
+	# virtual coordinate system is X*Y ${#DATA} * 5
+
+	REAL_OFFSET_X=0
+	REAL_OFFSET_Y=0
+
+	draw_char() {
+	V_COORD_X=$1
+	V_COORD_Y=$2
+
+	tput cup $((REAL_OFFSET_Y + V_COORD_Y)) $((REAL_OFFSET_X + V_COORD_X))
+
+	printf %c "${DATA[V_COORD_Y]:V_COORD_X:1}"
+	}
+
+
+	trap 'exit 1' INT TERM
+	trap 'tput setaf 9; tput cvvis; clear' EXIT
+
+	tput civis
+	clear
+
+	while :; do
+
+	for ((c=1; c <= 7; c++)); do
+	tput setaf $c
+	for ((x=0; x<${#DATA[0]}; x++)); do
+		for ((y=0; y<=4; y++)); do
+		draw_char $x $y
+		done
+	done
+	done
+
+	done
+}
+
 echo "=====> [*] is to Stringify =====>"
 echo "=====> [@] w/ QUOTES IS ITERABLE AND BEST! =====>"
 
@@ -47,3 +89,28 @@ for key in "${!arr[@]}" ; do
 done
 
 unset arr
+
+# [=====> HOLDOVER YAML OBJECT PARSING FROM CAMBERUNNER.SH =====>
+	# declare -a meta_values ;
+	# yaml_args="$( yq '.configuration.meta.[]' runner.yaml | tr '\n' ', ')" ;
+	# meta_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
+	# IFS=,; read -ra meta_values <<< "$meta_args" ; unset yaml_args ;
+	# declare -a git_values ;
+	# yaml_args="$( yq '.configuration.git-args[]' runner.yaml | tr '\n' ', ')" ;
+	# git_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
+	# IFS=,; read -ra git_values <<< "$git_args" ; unset yaml_args ;
+	# declare -a rsync_values ;
+	# yaml_args="$( yq '.configuration.rsync-args[]' runner.yaml | tr '\n' ', ')" ;
+	# rsync_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
+	# IFS=,; read -ra rsync_values <<< "$rsync_args" ; unset yaml_args ;
+	# declare -a exclusion_values ;
+	# yaml_args="$( yq '.configuration.rsync-args.rsync-exclusions.[]' runner.yaml | tr '\n' ', ' )" ;
+	# exclusion_args="$(printf '%s,' "${yaml_args[@]}" | tr '\n' ', ' )" ;
+	# IFS=,; read -ra exclusion_values <<< "$exclusion_args" ; unset yaml_args ;
+	# cyan-echo "=====> TESTING YAML VALUES IFS =====>" ;
+	# (IFS=,; echo "${meta_values[*]}"; ) ;
+	# (IFS=,; echo "${git_values[*]}"; ) ;
+	# (IFS=,; echo "${rsync_values[*]}"; ) ;
+	# (IFS=,; echo "${exclusion_values[*]}"; ) ;
+	# unset IFS;
+# <=====]
